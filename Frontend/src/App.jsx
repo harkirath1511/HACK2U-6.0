7069c14bee1c25b840bcf2380e4b67ff0b7ab99b
Harkirat Singh <singharkirath1511@gmail.com>
2025-01-22 23:01:26 +0530

    commit1

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div className='p-4 text-white bg-black'>bund pard page</div>
+    </>
+  )
+}
+
+export default App
