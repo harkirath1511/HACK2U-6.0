e6df85245a6627914218157bb5bef5359efa7d73
lendrik-kumar <sanketkamboj2006@gmail.com>
2025-01-23 14:41:38 +0530

    Refactor frontend structure: remove App.css, integrate Login component, and update main entry point

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Login from './pages/templates/login.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
-     <div className='p-4 text-white bg-black'>bund pard page</div>
+      <Login/>
+       
     </>
   )
 }
